1df3407ae562002ca7e4b03db2226030217ee0cf
Scott Hyndman <scotty.hyndman@gmail.com>
2025-04-06 21:11:31 -0700

    Add a compiler pre-commit check to husky

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,6 +4,7 @@ import jsonPlugin from '@rollup/plugin-json';
 import resolvePlugin from '@rollup/plugin-node-resolve';
 import replacePlugin from '@rollup/plugin-replace';
 import { isAbsolute, relative, resolve } from 'path';
+import { env } from 'process';
 import { readPackageUp } from 'read-package-up';
 import { defineConfig } from 'rollup';
 
@@ -11,62 +12,70 @@ import userscript from 'rollup-plugin-userscript';
 
 const { packageJson } = await readPackageUp();
 const extensions = ['.ts', '.tsx', '.mjs', '.js', '.jsx'];
+const noEmit = 'NO_EMIT' in env;
 
 export default defineConfig(
   Object.entries({
     'llmop': 'src/llmop/index.ts',
-  }).map(([name, entry]) => ({
-    input: entry,
-    plugins: [
-      babelPlugin({
-        // import helpers from '@babel/runtime'
-        babelHelpers: 'runtime',
-        plugins: [
-          [
-            import.meta.resolve('@babel/plugin-transform-runtime'),
-            {
-              useESModules: true,
-              version: '^7.5.0', // see https://github.com/babel/babel/issues/10261#issuecomment-514687857
-            },
+  }).map(([name, entry]) => {
+    const config = {
+      input: entry,
+      plugins: [
+        babelPlugin({
+          // import helpers from '@babel/runtime'
+          babelHelpers: 'runtime',
+          plugins: [
+            [
+              import.meta.resolve('@babel/plugin-transform-runtime'),
+              {
+                useESModules: true,
+                version: '^7.5.0', // see https://github.com/babel/babel/issues/10261#issuecomment-514687857
+              },
+            ],
           ],
-        ],
-        exclude: 'node_modules/**',
-        extensions,
-      }),
-      replacePlugin({
-        values: {
-          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+          exclude: 'node_modules/**',
+          extensions,
+        }),
+        replacePlugin({
+          values: {
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+          },
+          preventAssignment: true,
+        }),
+        resolvePlugin({ browser: false, extensions }),
+        commonjsPlugin(),
+        jsonPlugin(),
+        userscript((meta) =>
+          meta.replace('process.env.AUTHOR', packageJson.author.name),
+        ),
+      ],
+      external: defineExternal([
+        '@violentmonkey/ui',
+        '@violentmonkey/dom',
+        'solid-js',
+        'solid-js/web',
+      ]),
+      output: {
+        format: 'iife',
+        file: `dist/${name}.user.js`,
+        globals: {
+          // Note:
+          // - VM.solid is just a third-party UMD bundle for solid-js since there is no official one
+          // - If you don't want to use it, just remove `solid-js` related packages from `external`, `globals` and the `meta.js` file.
+          'solid-js': 'VM.solid',
+          'solid-js/web': 'VM.solid.web',
+          '@violentmonkey/dom': 'VM',
+          '@violentmonkey/ui': 'VM',
         },
-        preventAssignment: true,
-      }),
-      resolvePlugin({ browser: false, extensions }),
-      commonjsPlugin(),
-      jsonPlugin(),
-      userscript((meta) =>
-        meta.replace('process.env.AUTHOR', packageJson.author.name),
-      ),
-    ],
-    external: defineExternal([
-      '@violentmonkey/ui',
-      '@violentmonkey/dom',
-      'solid-js',
-      'solid-js/web',
-    ]),
-    output: {
-      format: 'iife',
-      file: `dist/${name}.user.js`,
-      globals: {
-        // Note:
-        // - VM.solid is just a third-party UMD bundle for solid-js since there is no official one
-        // - If you don't want to use it, just remove `solid-js` related packages from `external`, `globals` and the `meta.js` file.
-        'solid-js': 'VM.solid',
-        'solid-js/web': 'VM.solid.web',
-        '@violentmonkey/dom': 'VM',
-        '@violentmonkey/ui': 'VM',
+        indent: false,
       },
-      indent: false,
-    },
-  })),
+    };
+    if (noEmit) {
+      delete config.output;
+    }
+    return config;
+  }
+  ),
 );
 
 function defineExternal(externals) {
